ec79ae4e5569c62a30e1f405e5655e225499c641
jimipepper <maitre.jiraya@live.fr>
2015-01-12 03:25:04 +0100

    Add comments and scaladoc for package object

--- src/main/scala/opl/iagl/alloy/package.scala
+++ src/main/scala/opl/iagl/alloy/package.scala
@@ -1,19 +1,22 @@
 package opl.iagl
 
+// java
 import java.io.{PrintWriter, File}
 
+// alloy
 import edu.mit.csail.sdg.alloy4compiler.translator.A4Solution
+
+// intern project dependencies
 import opl.iagl.EnvInfo.ENVINFO
 import opl.iagl.Move.MOVE
 import opl.iagl.alloy.util.AlloyOutputParser
+
+// scala
 import scala.collection.immutable
 
 /**
- * Contains default classes to generate Picobot Java rules
- *
- * @author Romain Philippon
+ * Enumerates possible move value that Picobot can execute
  */
-
 object Move extends Enumeration {
   type MOVE = Value
   val NORTH = Value("N")
@@ -22,6 +25,13 @@ object Move extends Enumeration {
   val SOUTH = Value("S")
   val STAY = Value("x")
 
+  /**
+   * Gives a MOVE value from a character. It raises an exception if
+   * direction parameter is wrong
+   *
+   * @param direction Is a character representing a MOVE value
+   * @return the corresponding MOVE value from direction variable
+   */
   def get(direction : String) : MOVE = {
     direction match {
       case "N" => NORTH
@@ -29,10 +39,14 @@ object Move extends Enumeration {
       case "W" => WEST
       case "S" => SOUTH
       case "x" => STAY
+      case _ => throw new Exception("Can't find a MOVE value with "+ direction)
     }
   }
 }
 
+/**
+ * Enumerates possible environment value where Picobot is located
+ */
 object EnvInfo extends Enumeration {
   type ENVINFO = Value
   val NORTH = Value("N")
@@ -42,6 +56,13 @@ object EnvInfo extends Enumeration {
   val NOTHING = Value("x")
   val ANY = Value("*")
 
+  /**
+   * Gives a ENVINFO value from a character. It raises an exception if
+   * direction parameter is wrong
+   *
+   * @param direction Is a character representing a ENVINFO value
+   * @return the corresponding ENVINFO value from direction variable
+   */
   def get(direction : String) : ENVINFO = {
     direction match {
       case "N" => NORTH
@@ -50,15 +71,35 @@ object EnvInfo extends Enumeration {
       case "S" => SOUTH
       case "x" => NOTHING
       case "*" => ANY
+      case _ => throw new Exception("Can't find an ENVINFO value with "+ direction)
     }
   }
 }
 
+/**
+ * Describes a Picobot environment while crossing
+ *
+ * @see [opl.iagl.alloy.EnvInfo]
+ *
+ * @param north is the value information for the direction
+ * @param east is the value information for the direction
+ * @param west is the value information for the direction
+ * @param south is the value information for the direction
+ */
 case class Surroundings(north: ENVINFO, east: ENVINFO, west: ENVINFO, south: ENVINFO) {
   override def toString : String = {
     north.toString + east.toString + west.toString + south.toString
   }
 }
+
+/**
+ * Describes a Picobot action
+ *
+ * @see [opl.iagl.alloy.Move]
+ *
+ * @param nextState tells what is the next Picobot's intern state
+ * @param move tells what move the Picobot has to perform
+ */
 case class Action(nextState : Int, move : MOVE) {
   override def toString : String = {
     val direction = move match {
@@ -72,15 +113,40 @@ case class Action(nextState : Int, move : MOVE) {
     direction +" "+ nextState
   }
 }
+
+/**
+ * Describes a Picobot's rule
+ *
+ * @param currentState tells what is the Picobot's current intern state
+ * @param environment tells what the current environment where the Picobot is located
+ * @param next // tells, accordingly to the current current and the environment, the action that Picobot must perform
+ */
 case class Rule(currentState : Int, environment: Surroundings, next: Action) {
   override def toString : String = {
     currentState +" "+ environment.toString +" -> "+ next.toString
   }
 }
 
+/**
+ * Contains default classes to generate Picobot Java rules and helper funtions to build rules
+ * from an alloy result output
+ *
+ * @author Romain Philippon
+ */
 package object alloy {
-  val SEPARATOR_CHAR = '='
+  /**
+   * Is a character that separated alloy variable from its value
+   */
+  private val SEPARATOR_CHAR = '='
 
+  /**
+   * Parses an alloy output from Picobot.als model
+   *
+   * @see [opl.iag.alloy.Main.getFirstNSolutions]
+   *
+   * @param itSolution is the alloy solution provided by Alloy
+   * @return the corresponding list of rules that itSolution variable provided
+   */
   def parseRegularAlloyModel(itSolution: A4Solution): List[Rule] = {
     val solution = itSolution.toString.replace("$", "")
     val lines = solution.split("\n").drop(7).reverse.drop(16).reverse // delete useless line
@@ -181,6 +247,14 @@ package object alloy {
     rules.toList
   }
 
+  /**
+   * Parses an alloy output from StarPicobot.als model
+   *
+   * @see [opl.iag.alloy.Main.getFirstNSolutions]
+   *
+   * @param itSolution is the alloy solution provided by Alloy
+   * @return the corresponding list of rules that itSolution variable provided
+   */
   def parseStarAlloyModel(itSolution: A4Solution): List[Rule] = {
     val solution = itSolution.toString.replace("$", "")
     val lines = solution.split("\n").drop(7).reverse.drop(17).reverse // delete useless line
@@ -281,6 +355,13 @@ package object alloy {
     rules.toList
   }
 
+  /**
+   * Saves rules generated by Alloy in the temp system folder
+   *
+   * @param filename is the rules filename
+   * @param rules is the list of rules which will be saved
+   * @return the rule file with rules formatted for Picobot simulator
+   */
   def saveRules(filename : String, rules : List[Rule]) : File = {
     /* SAVE GENERATED PICOBOT'S RULES  */
     val picobotRuleFile = new File(System.getProperty("java.io.tmpdir") + File.separator + filename)
@@ -291,6 +372,7 @@ package object alloy {
 
     val writer = new PrintWriter(picobotRuleFile)
 
+    /* PRINT EACH RULE */
     for (rule <- rules) {
       writer.write(rule.toString + "\n\n")
     }
@@ -299,6 +381,19 @@ package object alloy {
     picobotRuleFile
   }
 
+  /**
+   * Is a helper function that builds an Surroundings instance in case where StarPicobot.als model
+   * was used
+   *
+   * @see [opl.iagl.alloy.Surroundings]
+   *
+   * @param surroundingName is the surrroundings name from the alloy solution
+   * @param north is the north direction value
+   * @param east is the east direction value
+   * @param west is the west direction value
+   * @param south is the south direction value
+   * @return
+   */
   private def buildSurroundingStarModel(surroundingName: String, north: immutable.Map[String, String], east: immutable.Map[String, String], west: immutable.Map[String, String], south: immutable.Map[String, String]) : (String, Surroundings) = {
     val northDirection = north.get(surroundingName) match {
       case Some("True") => EnvInfo.NORTH
@@ -331,6 +426,19 @@ package object alloy {
     (surroundingName, Surroundings(northDirection, eastDirection, westDirection, southDirection))
   }
 
+  /**
+   * Is a helper function that builds an Surroundings instance in case where Picobot.als model
+   * was used
+   *
+   * @see [opl.iagl.alloy.Surroundings]
+   *
+   * @param surroundingName is the surrroundings name from the alloy solution
+   * @param north is the north direction value
+   * @param east is the east direction value
+   * @param west is the west direction value
+   * @param south is the south direction value
+   * @return
+   */
   private def buildSurroundingRegularModel(surroundingName: String, north: immutable.Map[String, String], east: immutable.Map[String, String], west: immutable.Map[String, String], south: immutable.Map[String, String]) : (String, Surroundings) = {
     val northDirection = north.get(surroundingName) match {
       case Some("True") => EnvInfo.NORTH
